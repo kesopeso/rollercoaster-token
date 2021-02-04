f9a7ddc3ee05f3264699ca52150450345a89e89f
rollercoasterfinance <info@rollercoaster.finance>
2021-02-04 10:43:04 +0100

    updates liquidity lock migration

--- migrations/4_liquidity_lock.js
+++ migrations/4_liquidity_lock.js
@@ -1,7 +1,8 @@
+const Token = artifacts.require('TokenMock');
 const LiquidityLock = artifacts.require('LiquidityLock');
 
 module.exports = async (deployer) => {
-    //const tokenAddress = '';
-    //const releaseTime = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30 * 6; // 6 months lockup
-    //await deployer.deploy(LiquidityLock, tokenAddress, releaseTime);
+    const token = await Token.deployed();
+    const releaseTime = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30 * 6; // 6 months lockup
+    await deployer.deploy(LiquidityLock, token.address, releaseTime);
 };
